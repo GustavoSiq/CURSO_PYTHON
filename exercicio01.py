744575c47a13d801b9a0db38eaf8d5fc095db4d1
Gustavo Alves Siqueira <gustavo.siqueira@unijuazeiro.edu.br>
2023-03-29 21:21:27 -0300

    Versão inicial de uma nova jornada

---
+++ exercicio01.py
@@ -0,0 +1,6 @@
+nome = 'Gustavo'
+altura = 1.64
+peso = 54
+imc = peso / (altura * altura)
+
+print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}.')
---
+++ radar.py
@@ -0,0 +1,6 @@
+velocidade = 61  # velocidade atual do carro
+local_carro = 100  # local em que o carro está na estrada
+
+RADAR_1 = 60  # velocidade máxima do radar 1
+LOCAL_1 = 100  # local onde o radar 1 está
+RADAR_RANGE = 1  # A distância onde o radar pega
