2c942083f1ae1f8908d68da13c1e3b1d4b517e64
Gustavo Siqueira <gustavosiqueira.97@gmail.com>
2024-06-13 21:54:32 -0300

    Inicio de uma Carreira - Primeiro Commit

--- exercicio01.py
+++ exercicio01.py
@@ -1,6 +1,6 @@
 nome = 'Gustavo'
-altura = 1.64
-peso = 54
+altura = 1.68
+peso = 62
 imc = peso / (altura * altura)
 
 print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}.')
--- radar.py
+++
@@ -1,6 +0,0 @@
-velocidade = 61  # velocidade atual do carro
-local_carro = 100  # local em que o carro está na estrada
-
-RADAR_1 = 60  # velocidade máxima do radar 1
-LOCAL_1 = 100  # local onde o radar 1 está
-RADAR_RANGE = 1  # A distância onde o radar pega
