69f8d1b3300a172cfe58fbf16d5a506fecb9f400
Gustavo Siqueira <gustavosiqueira.97@gmail.com>
2024-06-18 20:54:12 -0300

    Adicionando exercício03.py - Exercício sobre while

---
+++ exercicio03.py
@@ -0,0 +1,9 @@
+nome = input('Qual seu nome? ')
+contador = 0
+novo_nome = ''
+while contador < len(nome):
+    letra = nome[contador]
+    novo_nome += f'{letra}-'
+    contador += 1
+
+print(f'-{novo_nome}')
